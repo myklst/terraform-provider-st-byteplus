d392601db97277aab3292cb69c73a1253b38a4d8
ChernChienChong <chitchong99@gmail.com>
2025-01-02 16:25:55 +0800

    Add docs, goreleaser, and GH actions.

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -8,19 +8,4 @@ terraform {
 
 provider "st-byteplus" {
   region     = "ap-singapore-1"
-  access_key = "NOT_USED"
-  secret_key = "NOT_USED"
 }
-
-data "st-byteplus_cdn_domain" "example" {
-  domain_name = "www.example.com"
-
-  client_config {
-    access_key = "AKAPZjJjZWQwNzdkNTg1NGQwNzgyYTdhNzM4MjRiM2RmMzc"
-    secret_key = "WkRVeFlXSmhZbUUzWmpSbE5ETTVNams1TlRRellqSmxPV1F4TmpGak5XWQ=="
-  }
-}
-
-# output "cdn_domain_details" {
-#   value = data.st-byteplus_cdn_domain.example
-# }
